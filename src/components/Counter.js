f802115a933e7476a625462020cc5df1714d10bd
Maxime Salomon <maxime@croissant.io>
2019-03-04 14:56:18 +0100

    Stretch

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,13 +5,15 @@ import { bindActionCreators } from 'redux';
 
 class Counter extends Component {
     incrementIfOdd = () => {
-        // Stretch Problem: Implement an increment function that
-        // only increments if the counter value is odd
+        if(this.props.count %2) {
+            return this.props.increment();
+        }
     };
 
     incrementAsync = () => {
-        // Stretch Problem: Implement an increment function that
-        // increments after waiting for one second
+        setTimeout(() => {
+            return this.props.increment();
+        }, 1000);
     };
 
     render() {
@@ -24,14 +26,12 @@ class Counter extends Component {
                 <button onClick={() => this.props.decrement() }>
                     -
                 </button>
-                 {/* Uncomment these button tags if you got
-                around to implementing the extra credit functions */}
-                {/* <button onClick={this.incrementIfOdd}>
+                <button onClick={this.incrementIfOdd}>
                     Increment if odd
                 </button>
                 <button onClick={this.incrementAsync}>
                     Increment async
-                </button>  */}
+                </button> 
             </p>
         );
     }
